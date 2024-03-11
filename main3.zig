39ebeb0217fc44117820bc00a16be7dc0d795f8e
Oskar Hahn <mail@oshahn.de>
2024-03-11 23:23:50 +0100

    manipulate ignore

--- main3.zig
+++ main3.zig
@@ -37,26 +37,17 @@ const CandidateIdx = u32;
 pub fn count(allocator: mem.Allocator, seats: u32, candidate_count: u32, vote_count: u32, raw_votes: []const u32) ![]u32 {
     // TODO: validate poll
     const tie_rank = raw_votes[vote_count * candidate_count ..];
-    const votes = try sortVotes(allocator, candidate_count, vote_count, raw_votes);
-    defer {
-        // TODO: the memory if votes seems a bit fragmented.
-        for (votes) |vote| {
-            for (vote) |group| {
-                allocator.free(group);
-            }
-            allocator.free(vote);
-        }
-        allocator.free(votes);
-    }
+    var votes = try VoteList.init(allocator, candidate_count, vote_count, raw_votes);
+    defer votes.deinit(allocator);
 
     const ignore_data = try allocator.alloc(u32, candidate_count);
     defer allocator.free(ignore_data);
     var ignore = GrowList(u32).init(ignore_data);
 
-    var vote_weights = try initWeights(allocator, @intCast(votes.len));
+    var vote_weights = try initWeights(allocator, vote_count);
     defer allocator.free(vote_weights);
 
-    var highest_candidates = try allocator.alloc(?[]u32, votes.len);
+    var highest_candidates = try allocator.alloc(?CandidateGroup, vote_count);
     defer allocator.free(highest_candidates);
 
     var counted_votes = try allocator.alloc(?u64, candidate_count);
@@ -65,7 +56,7 @@ pub fn count(allocator: mem.Allocator, seats: u32, candidate_count: u32, vote_co
     var elected_candidates = try ElectedCandidateList.init(allocator, seats);
 
     while (true) {
-        getHighest(&highest_candidates, votes, ignore.items());
+        getHighest(&highest_candidates, &votes, ignore.items());
         const vote_sum = countVotes(&counted_votes, vote_weights, highest_candidates, ignore.items());
 
         if (vote_sum == 0) {
@@ -94,17 +85,22 @@ pub fn count(allocator: mem.Allocator, seats: u32, candidate_count: u32, vote_co
     return elected_candidates.finalize();
 }
 
-fn getHighest(result: *[]?[]u32, votes: []const [][]u32, ignore: []const u32) void {
-    for (votes, 0..) |vote, i| {
-        result.*[i] = for (vote) |group| {
-            if (!contains_list(group, ignore)) {
-                break group;
+fn getHighest(result: *[]?CandidateGroup, votes: *VoteList, ignore: []const CandidateIdx) void {
+    for (votes.data, 0..) |*vote, i| {
+        result.*[i] = for (vote.data[vote.start..]) |*group| {
+            switch (group.remove_ignore(ignore)) {
+                RemoveResult.IsNowEmpty => {
+                    vote.start += 1;
+                },
+                RemoveResult.NotEmpty => {
+                    break group.*;
+                },
             }
         } else null;
     }
 }
 
-fn countVotes(result: *[]?u64, vote_weights: []const u32, vote_groups: []const ?[]const u32, ignore: []CandidateIdx) u64 {
+fn countVotes(result: *[]?u64, vote_weights: []const u32, vote_groups: []const ?CandidateGroup, ignore: []CandidateIdx) u64 {
     @memset(result.*, 0);
     for (ignore) |candidate_idx| {
         result.*[candidate_idx] = null;
@@ -114,7 +110,7 @@ fn countVotes(result: *[]?u64, vote_weights: []const u32, vote_groups: []const ?
     for (vote_groups, 0..) |may_vote_group, i| {
         if (may_vote_group) |vote_group| {
             const weight = vote_weights[i] / vote_group.len;
-            for (vote_group) |candidate_idx| {
+            for (vote_group.data) |candidate_idx| {
                 if (result.*[candidate_idx]) |v| {
                     result.*[candidate_idx] = v + weight;
                 }
@@ -155,12 +151,12 @@ fn getWinnerAndLooser(counted: []const ?u64, tie_rank: []const CandidateIdx, quo
     return WinnerLooser{ .looser = lowest_index };
 }
 
-fn updateVoteWeights(vote_weights: *[]u32, highest_candidates: []const ?[]const u32, winner: Winner, quota: u32) void {
+fn updateVoteWeights(vote_weights: *[]u32, highest_candidates: []const ?CandidateGroup, winner: Winner, quota: u32) void {
     const surplus = winner.votes - quota;
 
     for (highest_candidates, 0..) |may_candidate_group, i| {
         if (may_candidate_group) |candidate_group| {
-            if (contains(candidate_group, winner.candidate_idx)) {
+            if (contains(candidate_group.data, winner.candidate_idx)) {
                 const x: u32 = vote_weights.*[i] / @as(u32, @intCast(candidate_group.len));
                 vote_weights.*[i] = vote_weights.*[i] - x + @as(u32, @intCast(x * surplus / winner.votes));
             }
@@ -174,140 +170,172 @@ fn initWeights(allocator: mem.Allocator, vote_count: u32) ![]u32 {
     return vote_weights;
 }
 
-fn sortVotes(allocator: mem.Allocator, candidate_count: u32, vote_count: u32, raw_votes: []const u32) ![][][]u32 {
-    var output = try allocator.alloc([][]u32, vote_count);
-    var pref_index = try allocator.alloc(PrefIndex, candidate_count);
-    defer allocator.free(pref_index);
+const RemoveResult = enum {
+    IsNowEmpty,
+    NotEmpty,
+};
 
-    var i: usize = 0;
-    while (i < vote_count) : (i += 1) {
-        const vote_idx = i * candidate_count;
-        const vote = raw_votes[vote_idx .. vote_idx + candidate_count];
+const CandidateGroup = struct {
+    data: []CandidateIdx,
+    len: usize,
 
-        for (vote, 0..) |pref, candidate_idx| {
-            pref_index[candidate_idx] = PrefIndex{ .amount = pref, .candidate = @intCast(candidate_idx) };
-        }
-        mem.sort(PrefIndex, pref_index, {}, cmpPrefIndex);
-        output[i] = try unifyPrefIndex(allocator, pref_index);
-    }
+    fn remove_ignore(self: *CandidateGroup, ignore: []const CandidateIdx) RemoveResult {
+        var idx: usize = self.len - 1;
+        while (true) : (idx -= 1) {
+            for (ignore) |ignore_element| {
+                if (ignore_element == self.data[idx]) {
+                    if (self.len == 1) {
+                        return RemoveResult.IsNowEmpty;
+                    }
 
-    return output;
-}
-
-test "sort votes" {
-    const allocator = std.testing.allocator;
-
-    const got = try sortVotes(allocator, 2, 3, &[_]u32{ 1, 2, 3, 3, 2, 1 });
-    defer {
-        // TODO: the memory if votes seems a bit fragmented.
-        for (got) |vote| {
-            for (vote) |group| {
-                allocator.free(group);
+                    self.data[idx] = self.data[self.len - 1];
+                    self.len -= 1;
+                    break;
+                }
             }
-            allocator.free(vote);
+
+            if (idx == 0) break;
         }
-        allocator.free(got);
+        return RemoveResult.NotEmpty;
     }
 
-    var parsed = try std.json.parseFromSlice(
-        [][][]u32,
-        allocator,
-        \\ [[[1],[0]], [[0,1]], [[0],[1]]]
-    ,
-        .{},
-    );
-    defer parsed.deinit();
+    test "remove an element" {
+        var data = [_]u32{ 1, 2, 3 };
+        var group = CandidateGroup{ .data = &data, .len = 3 };
+        const got = group.remove_ignore(&[_]CandidateIdx{ 1, 3 });
+        const new_data = group.data[0..group.len];
+        try std.testing.expectEqualSlices(u32, &[_]CandidateIdx{2}, new_data);
+        try std.testing.expectEqual(RemoveResult.NotEmpty, got);
+    }
+};
 
-    try std.testing.expectEqualDeep(parsed.value, got);
-}
+const Vote = struct {
+    data: []CandidateGroup,
+    start: usize,
+};
 
-test "sort votes with zero values" {
-    const allocator = std.testing.allocator;
+const VoteList = struct {
+    data: []Vote,
 
-    const got = try sortVotes(allocator, 2, 3, &[_]u32{ 0, 2, 0, 4, 0, 6 });
-    defer {
-        // TODO: the memory if votes seems a bit fragmented.
-        for (got) |vote| {
-            for (vote) |group| {
-                allocator.free(group);
+    fn init(allocator: mem.Allocator, candidate_count: u32, vote_count: u32, raw_votes: []const u32) !VoteList {
+        var output = try allocator.alloc(Vote, vote_count);
+        var pref_index = try allocator.alloc(PrefIndex, candidate_count);
+        defer allocator.free(pref_index);
+
+        var i: usize = 0;
+        while (i < vote_count) : (i += 1) {
+            const vote_idx = i * candidate_count;
+            const vote = raw_votes[vote_idx .. vote_idx + candidate_count];
+
+            for (vote, 0..) |pref, candidate_idx| {
+                pref_index[candidate_idx] = PrefIndex{ .amount = pref, .candidate = @intCast(candidate_idx) };
             }
-            allocator.free(vote);
+            mem.sort(PrefIndex, pref_index, {}, cmpPrefIndex);
+            output[i] = try unifyPrefIndex(allocator, pref_index);
         }
-        allocator.free(got);
+
+        return VoteList{ .data = output };
     }
 
-    var parsed = try std.json.parseFromSlice(
-        [][][]u32,
-        allocator,
-        \\[[[1]], [[1]], [[1]]]
-    ,
-        .{},
-    );
-    defer parsed.deinit();
-
-    try std.testing.expectEqualDeep(parsed.value, got);
-}
-
-test "same value" {
-    const allocator = std.testing.allocator;
-
-    const got = try sortVotes(allocator, 2, 3, &[_]u32{ 2, 2, 2, 2, 2, 2 });
-    defer {
+    fn deinit(self: VoteList, allocator: mem.Allocator) void {
         // TODO: the memory if votes seems a bit fragmented.
-        for (got) |vote| {
-            for (vote) |group| {
-                allocator.free(group);
+        for (self.data) |vote| {
+            for (vote.data) |group| {
+                allocator.free(group.data);
             }
-            allocator.free(vote);
+            allocator.free(vote.data);
         }
-        allocator.free(got);
+        allocator.free(self.data);
     }
 
-    var parsed = try std.json.parseFromSlice(
-        [][][]u32,
-        allocator,
-        \\[[[0,1]], [[0,1]], [[0,1]]]
-    ,
-        .{},
-    );
-    defer parsed.deinit();
+    const PrefIndex = struct { amount: u32, candidate: u32 };
 
-    try std.testing.expectEqualDeep(parsed.value, got);
-}
+    fn cmpPrefIndex(_: void, a: PrefIndex, b: PrefIndex) bool {
+        return a.amount > b.amount;
+    }
 
-const PrefIndex = struct { amount: u32, candidate: u32 };
+    fn unifyPrefIndex(allocator: mem.Allocator, pref_index: []const PrefIndex) !Vote {
+        var list = try ArrayList(CandidateGroup).initCapacity(allocator, pref_index.len);
 
-fn cmpPrefIndex(_: void, a: PrefIndex, b: PrefIndex) bool {
-    return a.amount > b.amount;
-}
-
-fn unifyPrefIndex(allocator: mem.Allocator, pref_index: []const PrefIndex) ![][]u32 {
-    var list = try ArrayList([]u32).initCapacity(allocator, pref_index.len);
-
-    var i: usize = 0;
-    while (i < pref_index.len) {
-        if (pref_index[i].amount == 0) {
-            break;
-        }
-
-        var same_items: usize = 1;
-        while (i + same_items < pref_index.len) : (same_items += 1) {
-            if (pref_index[i].amount != pref_index[i + same_items].amount) {
+        var i: usize = 0;
+        while (i < pref_index.len) {
+            if (pref_index[i].amount == 0) {
                 break;
             }
+
+            var same_items: usize = 1;
+            while (i + same_items < pref_index.len) : (same_items += 1) {
+                if (pref_index[i].amount != pref_index[i + same_items].amount) {
+                    break;
+                }
+            }
+
+            var group = try allocator.alloc(u32, same_items);
+            for (0..same_items) |j| {
+                group[j] = pref_index[i + j].candidate;
+            }
+
+            try list.append(CandidateGroup{ .data = group, .len = same_items });
+            i += same_items;
         }
 
-        var group = try allocator.alloc(u32, same_items);
-        for (0..same_items) |j| {
-            group[j] = pref_index[i + j].candidate;
-        }
-
-        try list.append(group);
-        i += same_items;
+        return Vote{ .data = try list.toOwnedSlice(), .start = 0 };
     }
+};
 
-    return list.toOwnedSlice();
-}
+// test "sort votes" {
+//     const allocator = std.testing.allocator;
+
+//     const got = try VoteList.init(allocator, 2, 3, &[_]u32{ 1, 2, 3, 3, 2, 1 });
+//     defer got.deinit(allocator);
+
+//     var parsed = try std.json.parseFromSlice(
+//         [][][]u32,
+//         allocator,
+//         \\ [[[1],[0]], [[0,1]], [[0],[1]]]
+//     ,
+//         .{},
+//     );
+//     defer parsed.deinit();
+
+//     try std.testing.expectEqualDeep(parsed.value, got.data);
+// }
+
+// test "sort votes with zero values" {
+//     const allocator = std.testing.allocator;
+
+//     const got = try VoteList.init(allocator, 2, 3, &[_]u32{ 0, 2, 0, 4, 0, 6 });
+//     defer got.deinit(allocator);
+
+//     var parsed = try std.json.parseFromSlice(
+//         [][][]u32,
+//         allocator,
+//         \\[[[1]], [[1]], [[1]]]
+//     ,
+//         .{},
+//     );
+//     defer parsed.deinit();
+
+//     try std.testing.expectEqualDeep(parsed.value, got.data);
+// }
+
+// test "same value" {
+//     const allocator = std.testing.allocator;
+
+//     const got = try VoteList.init(allocator, 2, 3, &[_]u32{ 2, 2, 2, 2, 2, 2 });
+//     defer got.deinit(allocator);
+
+//     var parsed = try std.json.parseFromSlice(
+//         [][][]u32,
+//         allocator,
+//         \\[[[0,1]], [[0,1]], [[0,1]]]
+//     ,
+//         .{},
+//     );
+//     defer parsed.deinit();
+
+//     try std.testing.expectEqualDeep(parsed.value, got.data);
+// }
 
 fn contains_list(a_list: []const u32, b_list: []const u32) bool {
     for (a_list) |a| {
